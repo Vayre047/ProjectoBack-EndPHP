2aee3f0b574d3adf8dcb43510d821b3fba064c40
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-10-18 16:12:32 +0100

    Remove button is working

--- Javascript/Controllers/controller-category.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$title = "FarmShop - Categorias";
-
-	$onecategory = $modelCategories->OneCategory($id);
-	
-	require("Views/view-category.php");
-
-?>
--- Javascript/Controllers/controller-season.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$title = "FarmShop - Estações";
-
-	$winterSeason = $modelSeasons->getSeasonalProducts();
-
-	require("Views/view-season.php");
-
-?>
